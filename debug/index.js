dad869a85a8c3d9410efb348ac5bfa1931cd8c8f
Jose {Andres} Prendiz <jprendiz@gmail.com>
2022-12-10 19:44:05 -0600

    definicion de function scope

--- debug/index.js
+++ debug/index.js
@@ -22,3 +22,6 @@ const fruits = () => {
     });
 }
 
+/*
+En JavaScript, el ámbito de función es el conjunto de reglas que determinan dónde y cómo se pueden utilizar las variables declaradas en una función. En general, las variables declaradas dentro de una función solo están disponibles dentro de esa función y no pueden ser accedidas desde el ámbito externo. Esto se conoce como ámbito de función porque se aplica a las variables declaradas dentro de una función. Si quieres utilizar una variable declarada dentro de una función en el ámbito externo, puedes hacerlo retornando la variable como parte del resultado de la función. De esta manera, puedes acceder a la variable desde el ámbito externo. Es importante entender el ámbito de función para evitar conflictos de nombres y mejorar la organización del código.
+*/
